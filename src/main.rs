bad4ebe47176eb4cbc6a070f301687712fc1590f
Lorenz Schüler <lorenz@saarow.de>
2024-02-12 14:35:32 +0100

    add DistIter

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ mod universe_guard;
 use crate::{
     dispatch::{tag_to_execute, tag_to_receive},
     macros::task,
+    traits::Task,
     universe_guard::UniverseGuard,
 };
 
@@ -22,31 +23,69 @@ fn main() {
     }
 }
 
+struct DistIter<'w, I: Iterator<Item = Box<dyn Task>>> {
+    inner: I,
+    init: bool,
+    send_count: usize,
+    recv_count: usize,
+    world: &'w SimpleCommunicator,
+}
+
+impl<'w, I: Iterator<Item = Box<dyn Task>>> Iterator for DistIter<'w, I> {
+    type Item = Box<dyn std::any::Any>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if !self.init {
+            for dest in 1..self.world.size() {
+                if let Some(task) = self.inner.next() {
+                    task.send(self.world.process_at_rank(dest));
+                    self.send_count += 1;
+                }
+            }
+        }
+        if self.recv_count < self.send_count {
+            let (msg, status) = self.world.any_process().matched_probe();
+
+            let receive = tag_to_receive(status.tag());
+            let result = receive(msg);
+            self.recv_count += 1;
+
+            if let Some(task) = self.inner.next() {
+                task.send(self.world.process_at_rank(status.source_rank()));
+                self.send_count += 1;
+            }
+            Some(result)
+        } else {
+            None
+        }
+    }
+}
+
+trait MyIterExt {
+    fn into_dist_iter(self, world: &SimpleCommunicator) -> DistIter<Self>
+    where
+        Self: Iterator<Item = Box<dyn Task>> + Sized;
+}
+
+impl<I: Iterator<Item = Box<dyn Task>>> MyIterExt for I {
+    fn into_dist_iter(self, world: &SimpleCommunicator) -> DistIter<Self> {
+        DistIter {
+            inner: self,
+            init: false,
+            send_count: 0,
+            recv_count: 0,
+            world,
+        }
+    }
+}
+
 fn master(world: &SimpleCommunicator) {
-    let mut work_queue = (0..10)
+    (0..10)
         .into_iter()
         .map(task!(2, i32, i32, |x| x * x))
-        .chain((0..10).into_iter().map(task!(1, u8, u8, |x| x * 2)));
-    let mut recv_queue = Vec::new();
-    let mut send_count = 0;
-    for dest in 1..world.size() {
-        if let Some(task) = work_queue.next() {
-            task.send(world.process_at_rank(dest));
-            send_count += 1;
-        }
-    }
-
-    while recv_queue.len() < send_count {
-        let (msg, status) = world.any_process().matched_probe();
-
-        let receive = tag_to_receive(status.tag());
-        recv_queue.push(receive(msg));
-
-        if let Some(task) = work_queue.next() {
-            task.send(world.process_at_rank(status.source_rank()));
-            send_count += 1;
-        }
-    }
+        .chain((0..10).into_iter().map(task!(1, u8, u8, |x| x * 2)))
+        .into_dist_iter(world)
+        .for_each(|_| println!("x"));
 }
 
 fn worker(world: &SimpleCommunicator) {
