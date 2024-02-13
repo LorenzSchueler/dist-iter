3a90fd7ccb2bcb3a8f7aea6a30f8309e49fd27ca
Lorenz Schüler <lorenz@saarow.de>
2024-02-13 17:11:14 +0100

    generate tags

--- example/src/main.rs
+++ example/src/main.rs
@@ -3,11 +3,11 @@ use dist_iter::{task, IntoDistIter};
 #[dist_iter::main]
 fn main() {
     (0..10)
-        .map(task!(1, i32, i32, |x| x * x))
+        .map(task!(i32, i32, |x| x * x))
         .into_dist_iter()
         .for_each(|v| println!("{v}"));
     (0..10)
-        .map(task!(2, u8, u8, |x| x * 2))
+        .map(task!(u8, u8, |x| x * 2))
         .into_dist_iter()
         .for_each(|v| println!("{v}"));
 }
