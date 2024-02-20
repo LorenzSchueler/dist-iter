22eb7409d51c50bdfa0baa0637269a73523e975e
Lorenz Schüler <lorenz@saarow.de>
2024-02-20 14:49:18 +0100

    expose UninitBuffer to closure and add support for inplace mutation

--- dist-iter/examples/map.rs
+++ dist-iter/examples/map.rs
@@ -1,3 +1,5 @@
+use std::ops::DerefMut;
+
 use dist_iter::{map_iter_task, map_task, DistIterator, IntoDistIterator};
 
 #[dist_iter::main]
@@ -16,10 +18,7 @@ fn main() {
         .into_dist_iter::<2>()
         //.map_iter(map_iter_task!(2, i32, i32, |iter| { iter.map(|x| x * x) }))
         .map_iter(map_iter_task!(
-            2,
-            |iter: impl ExactSizeIterator<Item = i32>| -> impl IntoIterator<Item = i32> {
-                iter.map(|x| x * x)
-            }
+            |iter: UninitBuffer<i32, 2>| -> impl IntoIterator<Item = i32> { iter.map(|x| x * x) }
         ))
         .collect();
     results.sort();
@@ -31,8 +30,7 @@ fn main() {
     let mut results: Vec<_> = [1, 2, 3, 4, 5]
         .into_dist_iter::<2>()
         .map_iter(map_iter_task!(
-            2,
-            |iter: impl ExactSizeIterator<Item = i32>| -> impl IntoIterator<Item = i32> {
+            |iter: UninitBuffer<i32, 2>| -> impl IntoIterator<Item = i32> {
                 iter.map(|x| x * x).filter(|x| x % 2 == 0)
             }
         ))
@@ -41,6 +39,34 @@ fn main() {
 
     eprintln!("{results:?}");
     assert_eq!(results, [4, 16]);
+
+    // map_iter with multiple adapters inside and single return value
+    let results = [1, 2, 3, 4, 5]
+        .into_dist_iter::<2>()
+        .map_iter(map_iter_task!(
+            |iter: UninitBuffer<i32, 2>| -> impl IntoIterator<Item = i32> {
+                let sum = iter.map(|x| x * x).filter(|x| x % 2 == 0).sum::<i32>();
+                std::iter::once(sum)
+            }
+        ))
+        .sum::<i32>();
+
+    eprintln!("{results:?}");
+    assert_eq!(results, 20);
+
+    // map_iter with use of DerefMut
+    let mut results: Vec<_> = [1, 2, 3, 4, 5]
+        .into_dist_iter::<2>()
+        .map_iter(map_iter_task!(|buf: &mut UninitBuffer<i32, 2>| {
+            for item in buf.deref_mut() {
+                *item += 1;
+            }
+        }))
+        .collect();
+    results.sort();
+
+    eprintln!("{results:?}");
+    assert_eq!(results, [2, 3, 4, 5, 6]);
 
     //// map_iter with multiple adapters inside
     //let mut results: Vec<_> = [1, 2, 3, 4, 5]
@@ -53,19 +79,4 @@ fn main() {
 
     //eprintln!("{results:?}");
     //assert_eq!(results, [4, 16]);
-
-    // map_iter with multiple adapters inside and single return value
-    let results = [1, 2, 3, 4, 5]
-        .into_dist_iter::<2>()
-        .map_iter(map_iter_task!(
-            2,
-            |iter: impl ExactSizeIterator<Item = i32>| -> impl IntoIterator<Item = i32> {
-                let sum = iter.map(|x| x * x).filter(|x| x % 2 == 0).sum::<i32>();
-                std::iter::once(sum)
-            }
-        ))
-        .sum::<i32>();
-
-    eprintln!("{results:?}");
-    assert_eq!(results, 20);
 }
--- dist-iter/src/task.rs
+++ dist-iter/src/task.rs
@@ -86,7 +86,7 @@ macro_rules! map_task {
 
 #[macro_export]
 macro_rules! map_iter_task {
-    ($n: expr, |$closure_param:ident: impl ExactSizeIterator<Item = $in:ty>| -> impl IntoIterator<Item = $out:ty> $closure_block:block) => {{
+    (|$closure_param:ident: UninitBuffer<$in:ty, $n:literal>| -> impl IntoIterator<Item = $out:ty> $closure_block:block) => {{
         fn function(
             $closure_param: ::dist_iter::UninitBuffer<$in, $n>,
         ) -> impl IntoIterator<Item = $out> {
@@ -133,6 +133,53 @@ macro_rules! map_iter_task {
         impl ::dist_iter::MapIterTask<$n> for ThisTask {
             type In = $in;
             type Out = $out;
+
+            const TAG: ::dist_iter::mpi::Tag = ::dist_iter::gen_tag(file!(), line!(), column!());
+        }
+
+        ThisTask {}
+    }};
+    (|$closure_param:ident: &mut UninitBuffer<$in:ty, $n:literal>| $closure_block:block) => {{
+        fn function($closure_param: &mut ::dist_iter::UninitBuffer<$in, $n>) {
+            $closure_block
+        }
+
+        fn execute(
+            msg: ::dist_iter::mpi::point_to_point::Message,
+            status: ::dist_iter::mpi::point_to_point::Status,
+            process: ::dist_iter::mpi::topology::Process<
+                '_,
+                ::dist_iter::mpi::topology::SimpleCommunicator,
+            >,
+        ) -> ::dist_iter::WorkerMode {
+            use ::dist_iter::mpi::point_to_point::Destination;
+
+            let (mut buf, tag) = ::dist_iter::UninitBuffer::<_, $n>::from_matched_receive(msg);
+            eprintln!(
+                "    > [{}] data of length {:?}",
+                std::process::id(),
+                buf.len()
+            );
+            function(&mut buf);
+
+            eprintln!(
+                "    < [{}] data of length {:?}",
+                std::process::id(),
+                buf.len()
+            );
+            process.send_with_tag(&*buf, status.tag());
+            ::dist_iter::WorkerMode::Continue
+        }
+
+        #[linkme::distributed_slice(::dist_iter::FUNCTION_REGISTRY)]
+        static REGISTRY_ENTRY: ::dist_iter::RegistryEntry =
+            (<ThisTask as ::dist_iter::MapIterTask<$n>>::TAG, execute);
+
+        struct ThisTask {}
+
+        impl ::dist_iter::MapIterTask<$n> for ThisTask {
+            type In = $in;
+            type Out = $in;
 
             const TAG: ::dist_iter::mpi::Tag = ::dist_iter::gen_tag(file!(), line!(), column!());
         }
